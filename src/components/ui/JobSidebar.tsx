0b754843e47aab603f888f570efd6a7e8fb03a6a
rajnish <rajnish.singh@baxture.com>
2024-04-23 18:22:19 +0530

    Added job filter for location

--- src/components/ui/JobSidebar.tsx
+++ src/components/ui/JobSidebar.tsx
@@ -1,11 +1,60 @@
 import React from 'react'
+import { Label } from './label'
+import { Input } from './input'
+import Select from '@/components/ui/select'
+import prisma from '@/lib/prisma'
 
-const JobSidebar = () => {
+export default async function JobSidebar() {
+
+    async function filterjobs(formData: FormData) {
+
+        'use server'
+
+    }
+
+    const JobLocation = await prisma.job.findMany({
+        where: { approved: true },
+        select: { location: true },
+        distinct: ['location']
+    })
+
+    let location = JobLocation.map((job) => {
+        const { location } = job;
+        return location
+    })
+
+    location = location.filter(Boolean) as string[];
+
+
+
     return (
-        <aside className='md:w-[260px] sticky top-0 h-fit'>
-            sidebar
+        <aside className='md:w-[260px] p-4 sticky top-0 h-fit bg-background rounded-lg'>
+            <form action={filterjobs} >
+                <div className='space-y-4'>
+                    <div className='flex flex-col gap-2'>
+                        <Label htmlFor='jobName'>
+                            Search
+                        </Label>
+                        <Input id='jobName' name='jobName' placeholder='Title , Company , etc' />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <Label htmlFor='location'>
+                            location
+                        </Label>
+                        <Select id='location' name='location' value="">
+                            <option value="">All location</option>
+                            {
+                                location.map((location) => {
+                                    return <option key={location} value={location || ""}>{location}</option>
+                                })
+                            }
+                        </Select>
+
+                    </div>
+                </div>
+
+            </form>
         </aside>
     )
 }
 
-export default JobSidebar
---
+++ src/lib/job-Type.ts
@@ -0,0 +1,11 @@
+export const jobType =[
+"Full Time",
+"Part Time",
+"Contract",
+"Temporary",
+"Internship",
+"Volunteer"
+]
+
+
+export const locationType =["On-site" , "Remote" , "Hybrid"];
