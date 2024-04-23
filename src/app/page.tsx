0b14314c32fa1149ce3fcaa35ba2d03c7cbc8fc4
rajnish <rajnish.singh@baxture.com>
2024-04-23 13:35:33 +0530

    Added styles to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,22 @@ export default async function Home() {
   })
 
   return (
-    <main className="max-w-5xl m-auto px-5 my-10">
-      {
-        data.map((job) => {
-          return <JobList job={job} key={job.id} />
-        })
-      }
+    <main className="max-w-5xl m-auto px-5 my-10 space-y-10">
+      <div className="space-y-5 text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">Developer jobs</h1>
+        <p className="text-muted-foreground">Find your dream jobs</p>
+      </div>
+      <section>
+
+        <div className="space-y-4">
+
+          {
+            data.map((job) => {
+              return <JobList job={job} key={job.id} />
+            })
+          }
+        </div>
+      </section>
     </main>
   );
 }
